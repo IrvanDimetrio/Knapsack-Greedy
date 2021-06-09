fd62e89ec1352ec4a97e86809624c0f3919c9872
M. Irvan Dimetrio <52452132+IrvanDimetrio@users.noreply.github.com>
2021-06-09 17:51:32 +0700

    Create item.java

---
+++ item.java
@@ -0,0 +1,43 @@
+package knapsack_greedy;
+// Dibuat Oleh : Muhamad Irvan Dimetrio
+
+public class Item implements Comparable<Item> {
+
+    private int weight;
+    private int price;
+
+    public Item() {
+    }
+
+    public Item(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int compareTo(Item i) {
+        if (weight < i.getWeight()) {
+            return -1;
+        }
+        if (weight > i.getWeight()) {
+            return 1;
+        }
+        return 0;
+    }
+}
